a6019a98bdf2239298f0bd22594c7026720b26d5
vendredl <node.js_@kakao.com>
2023-07-27 23:06:38 +0900

    :tada: 프로젝트 시작
    프로젝트 시작

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import HeaderLayout from './layout/HeaderLayout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <><HeaderLayout /><Component {...pageProps} /></>
+  )
 }

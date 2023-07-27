383e269163585ba927328d50b3afbaad6057c5cb
vendredl <node.js_@kakao.com>
2023-07-27 23:27:14 +0900

    :heavy_plus_sign: Prisma / IronSession / ReactHookForm / SWR 추가
     Prisma / IronSession / ReactHookForm / SWR 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,14 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'music': "url('/img/music.jpg')",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+  corePlugins: {
+    aspectRatio: false,
+  },
 }

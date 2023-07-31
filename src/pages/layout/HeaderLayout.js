c6868c50eb66b414328080840f3fc8df78c00673
vendredl <node.js_@kakao.com>
2023-07-31 23:07:01 +0900

    :lipstick: Share 버튼 추가
    Share 버튼 추가

--- src/pages/layout/HeaderLayout.js
+++ src/pages/layout/HeaderLayout.js
@@ -1,20 +1,15 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import LogoutButton from '../components/Logout/LogoutButton';
+import { data } from 'autoprefixer';
 
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function HeaderLayout({ userData }) {
+export default function HeaderLayout() {
 
     const router = useRouter();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    // const user = fetcher('/api/user');
-
-    // // const userData = user.json()
-
-    // console.log(user)
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -24,19 +19,24 @@ export default function HeaderLayout({ userData }) {
             console.log(data)
             setIsLoggedIn(data.isLoggedIn);
         }
-
         fetchUser()
     })
 
+    if (!data) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div className="w-full h-16 bg-gray-900 fixed grid grid-cols-2">
             <div>
-
+               
             </div>
             <div className="flex justify-end content-center items-center">
                 {isLoggedIn ? (
-                    <LogoutButton />
+                    <>
+                        <button className="ml-2 mr-2 bg-gray-300 text-xs w-24 h-10 font-bold rounded-md"  onClick={() => router.push("/share")}>Share</button>
+                        <LogoutButton />
+                    </>
                 ) : (
                 <div>
                     <button className="text-white ml-2 mr-2 text-xs w-24 h-10" onClick={() => router.push("/log-in")}>Log In</button>

9770fc6904639d2722849af881c24e0fa02089e8
vendredl <node.js_@kakao.com>
2023-08-01 17:37:03 +0900

    :lipstick: 음악 검색 폼 추가
    음악 검색 폼 추가

--- src/pages/share.js
+++ src/pages/share.js
@@ -1,6 +1,7 @@
 import useSWR from 'swr';
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -10,6 +11,12 @@ export default function Share() {
 
     const router = useRouter();
 
+    const [artist, setArtist] = useState('')
+    const [artistData, setArtistData] = useState([])
+
+    const [songName, setSongName] = useState('')
+    const [songNameData, setSongNameData] = useState([])
+
     const {
         register,
         handleSubmit,
@@ -18,7 +25,59 @@ export default function Share() {
       
       const onSubmit = data => {
         // Your POST request here
+        // DB 저장 부분 추가
+
       };
+
+      const onChangeArtist = (e) => {
+        setArtist(e.target.value)
+        
+      }
+
+      const onChangeSongName = (e) => {
+        setSongName(e.target.value)
+        
+      }
+
+      const onClickArtist = async () => {
+
+        console.log(JSON.stringify(artist))
+
+        const fetchArtist = await fetch('/api/songs/artists', {
+            method: 'POST',
+            body: JSON.stringify({ artist }),
+            headers: {
+                'Content-Type': 'application/json',
+              },
+        })
+
+        const artistData = await fetchArtist.json()
+
+        console.log(artistData.results.artistmatches.artist)
+
+        setArtistData(artistData.results.artistmatches.artist.slice(0, 3))
+      }
+
+      const onClickSongName = async () => {
+
+        console.log(JSON.stringify(songName))
+
+        const fetchSongName = await fetch('/api/songs/song_names', {
+            method: 'POST',
+            body: JSON.stringify({ songName, artist }),
+            headers: {
+                'Content-Type': 'application/json',
+              },
+        })
+
+        const songNameData = await fetchSongName.json()
+
+        console.log(songNameData)
+
+        console.log(songNameData.results.trackmatches.track)
+
+        setSongNameData(songNameData.results.trackmatches.track.slice(0, 3))
+      }
     
     
     if (!user) {
@@ -30,32 +89,44 @@ export default function Share() {
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-items-center content-center items-center w-full" >
             <div className="w-2/6 h-12 m-8 flex ">
                 <div className="w-24 text-center mr-4 flex items-center">가수 이름</div>
-                <input {...register('artist', { required: "This field is required" })} placeholder="Artist Name" className="w-5/6 h-12 border-2 border-rose-600 pl-4" />
-                <button className="bg-gray-200 w-16 ml-4 rounded-2xl">검색</button>
+                <input {...register('artist', { required: "This field is required" })} placeholder="Artist Name" className="w-5/6 h-12 border-2 border-rose-600 pl-4" onChange={onChangeArtist} />
+                <div className="bg-gray-200 w-16 pl-2  ml-4 rounded-2xl flex items-center" onClick={onClickArtist}>검색</div>
+            </div>
+            <div className="flex justify-items-center content-center items-center ml-8">
+                {artistData ? artistData.map((artist) => (
+                        <button disabled key={artist.name} className="mr-4 active:text-red-400 active:font-bold">{artist.name}</button>
+                )) : null
+                }
             </div>
             {errors.artist && <span>{errors.artist.message}</span>}
             
             <div className="w-2/6 h-12 m-8 flex ">
                 <div className="w-24 text-center mr-4 flex items-center">곡 이름</div>
-                <input {...register('song', { required: "This field is required" })} placeholder="Song Name" className="w-5/6 h-12 border-2 border-rose-600 pl-4" />
-                <button className="bg-gray-200 w-16 ml-4 rounded-2xl">검색</button>
+                <input {...register('song', { required: "This field is required" })} placeholder="Song Name" className="w-5/6 h-12 border-2 border-rose-600 pl-4" onChange={onChangeSongName}  />
+                <div className="bg-gray-200 w-16 pl-2  ml-4 rounded-2xl flex items-center" onClick={onClickSongName}>검색</div>
+            </div>
+            <div className="flex justify-items-center content-center items-center ml-8">
+                {songNameData ? songNameData.map((song) => (
+                        <button disabled key={song.name} className="mr-4 active:text-red-400 active:font-bold">{song.name}</button>
+                )) : null
+                }
             </div>
             {errors.song && <span>{errors.song.message}</span>}
             <div className="w-2/6 h-12 m-8 flex">
                 <div className="w-24 text-center mr-4 flex items-center">곡 설명</div>
                 <input {...register('description', { required: "This field is required" })} placeholder="Song Description" className="w-5/6 h-12 border-2 border-rose-600 pl-4" />
-                <button className="w-16 ml-4"></button>
+                <button disabled className="w-16 ml-4"></button>
             </div>
             {errors.description && <span>{errors.description.message}</span>}
             
             <div className="w-2/6 h-12 m-8 flex">
                 <div className="w-24 text-center mr-4 flex items-center">장르</div>
                 <input {...register('genre', { required: "This field is required" })} placeholder="Genre" className="w-5/6 h-12 border-2 border-rose-600 pl-4" />
-                <button className="bg-gray-200 w-16 ml-4 rounded-2xl">검색</button>
+                <button disabled className="w-16 ml-4"></button>
             </div>
             {errors.genre && <span>{errors.genre.message}</span>}
             
-            <input type="submit" />
+            <input type="submit" className="w-1/6 h-12 bg-red-100 rounded-3xl" />
             </form>
         </div>
       );

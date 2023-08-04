ade0ed8d3588b108ff51973cc3511d95822c7a96
vendredl <node.js_@kakao.com>
2023-08-04 15:27:35 +0900

    :lipstick: 메인 화면에서 공유된 노래를 볼 수 있도록 추가
    메인 화면에서 공유된 노래를 볼 수 있도록 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,19 @@ export default function Home() {
 
   const { data: user, error: userError } = useSWR('/api/user', fetcher);
 
+  const { data: share, error: shareError } = useSWR('/api/songs/get_song', fetcher);
+
   const router = useRouter();
 
   console.log(user)
 
+  console.log(share)
+
   if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  if(!share){
     return <div>Loading...</div>;
   }
 
@@ -23,7 +31,19 @@ export default function Home() {
   
   return (
     <>
-      {user.email}
+      <div className="grid grid-cols-4 auto-rows-min w-full h-screen">
+        {share.map((share) => (
+          <div key={share.artist} className="bg-gray-100 p-4 m-4 rounded-lg mt-24 flex flex-col">
+            <h1 className="text-2xl font-bold m-2">{share.song}</h1>
+            <h2 className="text-xl font-bold m-2">{share.artist}</h2>
+            <p className="text-md m-2">{share.description}</p>
+            <p className="text-md m-2">#{share.genre.replace(",", " #")}</p>
+            <p className="text-md m-2">{share.name}</p>
+          </div>
+
+        ))
+        }
+      </div>
     </>
   )
 }

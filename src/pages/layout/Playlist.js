4712731a4136320789e0558613a59ba06c7b441c
vendredl <node.js_@kakao.com>
2023-08-14 16:21:24 +0900

    :lipstick: Playlist에 좋아요 누른 음악 보이도록 추가
    Playlist에 좋아요 누른 음악 보이도록 추가

--- src/pages/layout/Playlist.js
+++ src/pages/layout/Playlist.js
@@ -2,22 +2,52 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import LogoutButton from '../components/Logout/LogoutButton';
 import { data } from 'autoprefixer';
+import useSWR, { mutate } from "swr";
 
 
-// const fetcher = (url) => fetch(url).then((res) => res.json());
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+
 
 export default function Playlist() {
 
+    const { data: user, error: userError } = useSWR('/api/user', fetcher);
+
+    // const { data: share, error: shareError } = useSWR('/api/songs/get_song', fetcher);
+
     const [playlist, setPlaylist] = useState(false)
+
+    const [song, setSong] = useState([])
 
     const onClick = () => {
         setPlaylist(!playlist)
+        const fetchSong = async () => {
+            const res = await fetch('/api/songs/get_song');
+            const data = await res.json()
+
+            console.log(data)
+            setSong(data)
+        }
+
+        fetchSong()
+
     }
 
     return (
         <>
             {playlist ? (
-                <div className="fixed bottom-24 right-4 w-1/6 h-5/6 bg-white/20 rounded-3xl backdrop-blur-sm " id='playlist'></div>
+                <div className="fixed bottom-24 right-4 w-1/6 h-5/6 bg-white/50 rounded-3xl backdrop-blur-sm border-2 border-gray-300 z-50 grid grid-flow-row auto-rows-max" id='playlist'>
+                    {
+                        song.map((song) => (
+                            song.isLiked ? (
+                                <div key={song.id} className="flex w-full h-24 justify-center items-center">
+                                    <p className="m-2 text-md font-bold">{song.song}</p>
+                                    <p className="m-2 text-sm">{song.artist}</p>
+                                </div>
+                            ) : null
+                        ))
+                    }
+                </div>
             ) : (
                 null
             )}

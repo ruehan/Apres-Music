9d9f1ef8c696aaa54b519e8962c7d5220dfaebc1
vendredl <node.js_@kakao.com>
2023-08-21 23:32:38 +0900

    :alien:  API 변경 last.fm -> maniaDB
     API 변경 last.fm -> maniaDB

--- src/pages/api/songs/artists.js
+++ src/pages/api/songs/artists.js
@@ -4,8 +4,24 @@ const apiKey = process.env.API_KEY
 export default async function handler(req, res) {
     const { artist } = req.body;
 
-    const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artist}&api_key=${apiKey}&format=json`)
-    const artistData = await response.json()
+    // const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artist}&api_key=${apiKey}&format=json`)
+    // const artistData = await response.json()
 
-    res.json(artistData)
+
+    var convert = require("xml-js")
+
+    const response = await fetch(`https://www.maniadb.com/api/search/${artist}/?sr=artist&display=20&key=example&v=0.5`)
+    
+    const xmlString = await response.text()
+
+    // console.log(xmlString)
+
+    var xmlToJson = convert.xml2json(xmlString, {compact: true}).replaceAll("", "")
+
+    console.log(xmlToJson)
+
+    var xmlToJson = JSON.parse(xmlToJson.replaceAll("maniadb:", ""))
+
+    // res.json(artistData)
+    res.status(200).json(xmlToJson);
 }
